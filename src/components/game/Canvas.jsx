a3da5aa31fbb679fbff879f73bbd890046072e6f
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-30 17:58:26 +0200

    Remove test

--- src/components/game/Canvas.jsx
+++ src/components/game/Canvas.jsx
@@ -1,12 +1,7 @@
 import React, { Component } from 'react'
 
 export default class Canvas extends Component {
-  ctx
-
-  componentDidMount() {
-    this.ctx = this.refs.myCanvas.getContext('2d')
-    this.ctx.fillRect(0,0, 100, 100);
-  }
+  
   render() {
     return (
       <canvas ref='myCanvas' />
