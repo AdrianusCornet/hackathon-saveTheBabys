37bd2f889bca48f4c3f2058576ce6ed0b8925471
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-31 11:03:42 +0200

    Add working canvas

--- src/components/game/Canvas.jsx
+++ src/components/game/Canvas.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
 
 export default class Canvas extends Component {
-  
+  state ={
+    ctx: null
+  }
+
+  update() {
+    const ctx = this.state.ctx
+
+    ctx.beginPath();
+    ctx.rect(20, 40, 50, 50);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+    ctx.closePath();
+
+    requestAnimationFrame(() => this.update())
+  }
+
+  componentDidMount() {
+    const ctx = this.refs.myCanvas.getContext('2d')
+
+    this.setState({
+      ctx
+    })
+
+    requestAnimationFrame(() => this.update())
+  }
+
   render() {
     return (
-      <canvas ref='myCanvas' />
+      <canvas ref='myCanvas'
+        width={1000}
+        height={1000}
+      />
     )
   }
 }
