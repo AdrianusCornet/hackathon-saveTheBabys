882026dfbba79fe69230435e987387fcdbc90f0d
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-08-01 15:24:59 +0200

    Put player movement in separate functon

--- src/components/game/Canvas.jsx
+++ src/components/game/Canvas.jsx
@@ -15,32 +15,13 @@ export default class Canvas extends Component {
 
   mainLoop() {
     const ctx = this.state.ctx
-    const {horizontal, vertical} = this.state.inputManager.movment
 
     this.state.inputManager.pollGamepad()
 
     // game logic
-      let playerX = this.state.playerX + horizontal
-      let playerY = this.state.playerY + vertical
 
-      if (playerX < 0) {
-        playerX = 0
-      }
-      if(playerX + PLAYER_SIZE > this.state.screenX) {
-        playerX = this.state.screenX - PLAYER_SIZE
-      }
+    this.doPlayerMovement()
 
-      if (playerY < 0) {
-        playerY = 0
-      }
-      if(playerY + PLAYER_SIZE > this.state.screenY) {
-        playerY = this.state.screenY - PLAYER_SIZE
-      }
-   
-      this.setState({
-        playerX,
-        playerY,
-      })
     // draw
     // -background
     ctx.beginPath()
@@ -57,10 +38,34 @@ export default class Canvas extends Component {
 
     requestAnimationFrame(() => this.mainLoop())
   }
+  doPlayerMovement() {
+    const { horizontal, vertical } = this.state.inputManager.movment
+
+    let playerX = this.state.playerX + horizontal
+    let playerY = this.state.playerY + vertical
+
+    if (playerX < 0) {
+      playerX = 0
+    }
+    if (playerX + PLAYER_SIZE > this.state.screenX) {
+      playerX = this.state.screenX - PLAYER_SIZE
+    }
+
+    if (playerY < 0) {
+      playerY = 0
+    }
+    if (playerY + PLAYER_SIZE > this.state.screenY) {
+      playerY = this.state.screenY - PLAYER_SIZE
+    }
+    this.setState({
+      playerX,
+      playerY,
+    })
+  }
   componentDidMount() {
     // set up game
     const ctx = this.refs.myCanvas.getContext('2d')
-    
+
     this.state.inputManager.initEventLiseners()
 
     this.setState({
